1ecc45e8d438d585ef9a932392d06bf6c0bf80fd
sofianeo <sofiane_ouldamara@outlook.fr>
2024-03-12 15:02:37 +0100

    soa verficiation path

--- src/test.py
+++ src/test.py
@@ -6,3 +6,6 @@ print('hello')
 # Récupération du chemin du dossier logs
 logs_path = config['logs']
 print("Chemin du dossier logs:", logs_path)
+
+datasets_path = config['datasets']
+print("Chemin du dossier logs:", datasets_path)
